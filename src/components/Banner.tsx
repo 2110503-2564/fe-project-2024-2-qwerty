9ba94ecc690a67a294646eed0610f1cca4389ee3
pxnnnnll <apkgamming@gmail.com>
2025-03-24 21:37:30 +0700

    edit topmenu

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -17,7 +17,10 @@ export default function Banner () {
 			<Image src = {covers[index]}
 			alt = 'cover' 
 			fill = {true} 
-			objectFit = 'cover'/>
+			objectFit = 'cover'
+			priority
+			/>
+			
 			<div className = {styles.bannerText}></div>
 			{session?<div className='z-20 absolute top-5 right-10 font-semibold text-yellow-600 text-xl'>Welcome {session.user?.name}</div>:null}
 			{/*<button className = "text-center bg-white text-cyan-600 border border-cyan-600 font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-0 hover:bg-cyan-600 hover:text-white hover:border-transparent"
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -12,7 +12,17 @@ export default async function TopMenu() {
 
     return (
         <div className = {styles.menucontainer}>
-            <Image src = {'/img/logo.png'} className = {styles.logoimg} alt = 'logo' width = {0} height = {0} sizes = "100vh"/>
+        <Link href="/" className={styles.logoimg}>
+            <Image
+                src={'/img/logo.png'}
+                className={styles.logoimg}
+                alt="logo"
+                width={0}
+                height={0}
+                sizes="100vh"
+            />
+        </Link>
+            
             <TopMenuItem  title = 'Companies' pageRef = '/companies'/>
             {
                 session?<TopMenuItem  title = 'New Booking' pageRef = '/booking'/>:null
@@ -23,13 +33,13 @@ export default async function TopMenu() {
 
 
 
-            <div className='flex flex-row items-center absolute right-0 h-full px-5 text-cyan-600 text-sm'>
+            <div className='flex flex-row items-center absolute right-0 h-full px-5 text-cyan-600 '>
             {
                 session? <Link href = "/api/auth/signout">
                     <div className='flex items-center h-full px-5 text-black text-sm'>Sign-Out of {session.user?.name}</div>
                 </Link>
-                :<Link href = "/login">
-                    <div className='no-underline flex items-center h-full px-5 text-blue-600 text-xl border border-blue-600 font-sans rounded-lg hover:bg-blue-100 transition'>Sign-In</div>
+                :<Link style={{ textDecoration: 'none' }} href = "/login">
+                    <div className='no-underline w-full flex items-center h-full px-5 text-blue-600 text-xl font-bold border-2 border-blue-600 antialiased  font-sans rounded-lg hover:bg-blue-500 hover:text-white hover:border-blue-500 transition' >Sign-In</div>
                 </Link>
             }
             </div>
