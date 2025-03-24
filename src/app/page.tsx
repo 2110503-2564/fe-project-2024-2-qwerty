5e430ac1feaafefb77f0baac6905db5281c2531e
pxnnnnll <apkgamming@gmail.com>
2025-03-24 20:46:41 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Banner from "@/components/Banner";
-
+import Card2 from "@/components/Card2";
 export default function Home() {
   return (
     <main className={styles.main}>
       <Banner></Banner>
+      <h1 className="text-center mt-[20px] text-3xl">Online Job Fair</h1>
+      <h3 className="text-center">Oppotunity to find the job you dreamed of.</h3>
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+      <Card2
+        imageUrl="img/logo.png"
+        title='-----------------text-----------------'
+        buttonText="-----------------text-----------------"
+      />
+      <Card2
+        imageUrl="img/logo.png"
+        title='หางานที่ใช่ด้วย OLJF ตัวช่วยให้คุณสามารถหางานให้ได้ง่ายขึน'
+        buttonText="หางานฟรี!"
+      />
+    </div>
+      
     </main>
   );
 }
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -13,20 +13,23 @@ export default async function TopMenu() {
     return (
         <div className = {styles.menucontainer}>
             <Image src = {'/img/logo.png'} className = {styles.logoimg} alt = 'logo' width = {0} height = {0} sizes = "100vh"/>
-            <TopMenuItem title = 'Companies' pageRef = '/companies'/>
+            <TopMenuItem  title = 'Companies' pageRef = '/companies'/>
             {
-                session?<TopMenuItem title = 'New Booking' pageRef = '/booking'/>:null
+                session?<TopMenuItem  title = 'New Booking' pageRef = '/booking'/>:null
             }
             {
-                session?<TopMenuItem title = 'My Booking' pageRef = "/mybooking"></TopMenuItem>:null
+                session?<TopMenuItem  title = 'My Booking' pageRef = "/mybooking"></TopMenuItem>:null
             }
+
+
+
             <div className='flex flex-row items-center absolute right-0 h-full px-5 text-cyan-600 text-sm'>
             {
                 session? <Link href = "/api/auth/signout">
-                    <div className='flex items-center h-full px-5 text-cyan-600 text-sm'>Sign-Out of {session.user?.name}</div>
+                    <div className='flex items-center h-full px-5 text-black text-sm'>Sign-Out of {session.user?.name}</div>
                 </Link>
                 :<Link href = "/login">
-                    <div className='flex items-center h-full px-5 text-cyan-600 text-sm'>Sign-In</div>
+                    <div className='no-underline flex items-center h-full px-5 text-blue-600 text-xl border border-blue-600 font-sans rounded-lg hover:bg-blue-100 transition'>Sign-In</div>
                 </Link>
             }
             </div>
